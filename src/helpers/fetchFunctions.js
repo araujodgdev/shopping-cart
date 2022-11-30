65c83568cbf30bbfa1f2fd77dc3dd92823fbf7e6
Douglas Araújo <goudxs@gmail.com>
2022-11-30 00:59:09 -0300

    Adiciona o requisito 7

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -1,5 +1,12 @@
-export const fetchProduct = () => {
-  // seu código aqui
+export const fetchProduct = async (id) => {
+  if (id === undefined) throw new Error('ID não informado');
+
+  const PRODUCTINFO_API = `https://api.mercadolibre.com/items/${id}`;
+
+  const result = await fetch(PRODUCTINFO_API);
+  const data = await result.json();
+
+  return data;
 };
 
 export const fetchProductsList = async (termoDeBusca) => {
